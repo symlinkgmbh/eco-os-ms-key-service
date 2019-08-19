3716b3d8bc5e94a7932b29d95734a769569db035
Symlink GmbH <54237931+symlinkgmbh@users.noreply.github.com>
2019-08-20 00:33:26 +0200

    init

---
+++ src/api/controllers/KeyController.ts
@@ -0,0 +1,115 @@
+/** 
+* Copyright 2018-2019 Symlink GmbH 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+*  
+*     http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+* 
+*/ 
+
+import { Request } from "express";
+import { Key, injectKeyService } from "@symlinkde/eco-os-pk-storage-key";
+import { CustomRestError, apiResponseCodes } from "@symlinkde/eco-os-pk-api";
+import { injectUserClient } from "@symlinkde/eco-os-pk-core";
+import { PkCore, MsKey, PkStorageKey, MsOverride } from "@symlinkde/eco-os-pk-models";
+
+@injectUserClient
+@injectKeyService
+export class KeyController {
+  private keyService!: PkStorageKey.IKeyService;
+  private userClient!: PkCore.IEcoUserClient;
+  public async getUsersKeysByEmail(req: MsOverride.IRequest): Promise<Array<MsKey.IKey>> {
+    try {
+      const response = await this.userClient.loadUserByEmail(req.params.email);
+      if (response.data.isActive === false) {
+        throw new CustomRestError(
+          {
+            code: apiResponseCodes.C817.code,
+            message: apiResponseCodes.C817.message,
+          },
+          400,
+        );
+      }
+    } catch (err) {
+      throw new CustomRestError(
+        {
+          code: apiResponseCodes.C818.code,
+          message: apiResponseCodes.C818.message,
+        },
+        400,
+      );
+    }
+
+    const result = await this.keyService.getPubKeys(req.params.email);
+
+    if (result === null || result.length < 1) {
+      throw new CustomRestError(
+        {
+          code: apiResponseCodes.C819.code,
+          message: apiResponseCodes.C819.message,
+        },
+        404,
+      );
+    }
+
+    return result;
+  }
+
+  public async revokeKeysFromUserByKey(req: MsOverride.IRequest): Promise<void> {
+    await this.keyService.revokePubKeyByKey(req.params.pubKey);
+  }
+
+  public async revokeKeyFromUserByDeviceId(req: MsOverride.IRequest): Promise<void> {
+    await this.keyService.revokePubKeyByDeviceId(req.params.deviceId);
+  }
+
+  public async revokeAllKeys(): Promise<void> {
+    await this.keyService.revokeAllPubKeys();
+  }
+
+  public async addKeyToUserByEmail(req: Request): Promise<void> {
+    const key: MsKey.IKey = req.body;
+    const exists = await this.keyService.checkForKeyAndDeviceId(key.pubKey, key.deviceId);
+    if (exists) {
+      throw new CustomRestError(
+        {
+          code: apiResponseCodes.C820.code,
+          message: apiResponseCodes.C820.message,
+        },
+        400,
+      );
+    }
+    const result = await this.keyService.addPubKey(key.email, key.pubKey, key.deviceId);
+    if (result === null) {
+      throw new CustomRestError(
+        {
+          code: 400,
+          message: "Key could not be created",
+        },
+        400,
+      );
+    }
+  }
+
+  public async revokeKeysFromUserByEmail(req: MsOverride.IRequest): Promise<void> {
+    try {
+      await this.keyService.revokePubKeys(req.params.email);
+    } catch (err) {
+      throw new CustomRestError(
+        {
+          code: 400,
+          message: "Problem in delete key",
+        },
+        400,
+      );
+    }
+  }
+}
---
+++ src/api/routes/Key.ts
@@ -0,0 +1,139 @@
+/** 
+* Copyright 2018-2019 Symlink GmbH 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+*  
+*     http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+* 
+*/ 
+
+import { AbstractRoutes, injectValidatorService } from "@symlinkde/eco-os-pk-api";
+import { PkApi } from "@symlinkde/eco-os-pk-models";
+import { Application, Request, Response, NextFunction } from "express";
+import { KeyController } from "../controllers";
+
+@injectValidatorService
+export class KeyRoute extends AbstractRoutes implements PkApi.IRoute {
+  private keyController: KeyController;
+  private validatorService!: PkApi.IValidator;
+  private postKeyPattern: PkApi.IValidatorPattern = {
+    pubKey: "",
+    deviceId: "",
+    email: "",
+  };
+
+  constructor(app: Application) {
+    super(app);
+    this.keyController = new KeyController();
+    this.activate();
+  }
+
+  public activate(): void {
+    this.getUsersKeysByEmail();
+    this.revokeKeysFromUserByEmail();
+    this.revokeKeysFromUserByKey();
+    this.revokeKeyFromUserByDeviceId();
+    this.revokeAllKeys();
+    this.addKeyToUserByEmail();
+  }
+
+  private getUsersKeysByEmail(): void {
+    this.getApp()
+      .route("/key/:email")
+      .get((req: Request, res: Response, next: NextFunction) => {
+        this.keyController
+          .getUsersKeysByEmail(req)
+          .then((result) => {
+            res.send(result);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+  }
+
+  private revokeKeysFromUserByEmail(): void {
+    this.getApp()
+      .route("/key/:email")
+      .delete((req: Request, res: Response, next: NextFunction) => {
+        this.keyController
+          .revokeKeysFromUserByEmail(req)
+          .then((result) => {
+            res.send(result);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+  }
+
+  private revokeKeysFromUserByKey(): void {
+    this.getApp()
+      .route("/revoke/key/:pubKey")
+      .delete((req: Request, res: Response, next: NextFunction) => {
+        this.keyController
+          .revokeKeysFromUserByKey(req)
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+  }
+
+  private revokeKeyFromUserByDeviceId(): void {
+    this.getApp()
+      .route("/revoke/device/:deviceId")
+      .delete((req: Request, res: Response, next: NextFunction) => {
+        this.keyController
+          .revokeKeyFromUserByDeviceId(req)
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+  }
+
+  private revokeAllKeys(): void {
+    this.getApp()
+      .route("/keys")
+      .delete((req: Request, res: Response, next: NextFunction) => {
+        this.keyController
+          .revokeAllKeys()
+          .then(() => {
+            res.sendStatus(200);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+  }
+
+  private addKeyToUserByEmail(): void {
+    this.getApp()
+      .route("/key")
+      .post((req: Request, res: Response, next: NextFunction) => {
+        this.validatorService.validate(req.body, this.postKeyPattern);
+        this.keyController
+          .addKeyToUserByEmail(req)
+          .then((result) => {
+            res.send(result);
+          })
+          .catch((err) => {
+            next(err);
+          });
+      });
+    return;
+  }
+}
